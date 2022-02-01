454ec38a4f1a6557f7919849b7adfc389c2e60fe
GreedyQ <lucasperese@gmail.com>
2022-01-31 18:33:27 -0600

    created fanduel file

---
+++ fanduel.py
@@ -0,0 +1,8 @@
+from optparse import TitledHelpFormatter
+from bs4 import BeautifulSoup
+import urllib.request
+#import re
+from scraping import oddsAndTeamsExtractor
+from scraping import gamesDictCreator
+
+url = "https://sportsbook.draftkings.com/leagues/basketball/88670846"
