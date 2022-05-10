f1cbaebc39d6a87d54de6ae483150f03e18aadd9
Tenzin Choezin <tchoezin17@gmail.com>
2022-05-09 20:05:46 -0400

    some selenium work on fanduel

--- fanduel.py
+++ fanduel.py
@@ -1,8 +1,24 @@
-from optparse import TitledHelpFormatter
-from bs4 import BeautifulSoup
-import urllib.request
-#import re
-from scraping import oddsAndTeamsExtractor
-from scraping import gamesDictCreator
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-url = "https://sportsbook.draftkings.com/leagues/basketball/88670846"
+
+options = Options()
+options.headless = True
+options.add_argument('window-size=1920x1080')
+
+url = 'https://sportsbook.fanduel.com/navigation/mlb'
+path = '/Users/tenzinchoezin/Desktop/chromedriver'
+
+driver = webdriver.Chrome(path, options=options)
+driver.get(url)
+
+
+odds = driver.find_elements_by_xpath('//span[@class="iq ir fo fh ja jb dz"]')
+for odd in odds:
+    print(odd.text)
+driver.quit()
